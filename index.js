a42d2a8bfa3a505c5668f31f8afd9901113639d2
lowlighter <simon.lecoq@live.fr>
2019-06-09 22:27:50 -0400

    Support for rotation

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
   const fs = require("fs")
   const fse = require("fs-extra")
   const sharp = require("sharp")
-  const argv = require("minimist")(process.argv.slice(2), {alias:{s:"size", o:"overlap"}, default:{size:256, overlap:0}})
+  const argv = require("minimist")(process.argv.slice(2), {alias:{s:"size", o:"overlap", r:"rotate"}, default:{size:256, overlap:0, rotate:true}})
   const progress = require("cli-progress")
   
 //Generate images
-  if (fs.existsSync("raw")) {
+  if (fs.existsSync("raw2")) {
     //Init
-      let { size, overlap } = argv
-      console.log(`size: ${size}x${size}\noverlap: ${overlap*100}%`)
+      let { size, overlap, rotate } = argv
+      console.log(`size: ${size}x${size}\noverlap: ${overlap*100}%\nrotate: ${rotate}`)
       let bar = new progress.Bar({}, progress.Presets.shades_classic)
 
     //UUID generator
@@ -23,18 +23,27 @@
       fse.emptyDirSync("computed")
 
     //Generate images
-      let files = fs.readdirSync("raw")
+      let files = fs.readdirSync("raw2")
       console.log(`files: ${files.length}`)
       bar.start(files.length, 0)
       files.forEach(async file => {
         //Prepare image
-          let image = sharp(`raw/${file}`)
+          let image = sharp(`raw2/${file}`)
           let {width, height} = await image.metadata()
 
         //Generate sub-images
           for (let x = 0; x + size < width; x = Math.floor(x + size * (1 - overlap)))
-            for (let y = 0; y + size < height; y = Math.floor(y + size * (1 - overlap))) 
-              image.extract({left:x, top:y, width:size, height:size}).toFile(`computed/${file.replace(/\.(.+?$)/, (m, a) => `-${uuid()}.${a}`)}`)
+            for (let y = 0; y + size < height; y = Math.floor(y + size * (1 - overlap))) {
+              let output = `computed/${file.replace(/\.(.+?$)/, (m, a) => `-${uuid()}.${a}`)}`
+              await image.extract({left:x, top:y, width:size, height:size}).jpeg({quality:100, chromaSubsampling:"4:4:4"}).toFile(output)
+              //Rotations
+                for (let r = 1; r < (rotate ? 4 : 1); r++)
+                  await sharp(output).rotate(r*90).toFile(output.replace(/-[0-9a-f]+\.(.+?$)/, (m, a) => `-${uuid()}.${a}`))
+            } 
+              
+        //Update bar
           bar.increment()
+          if (bar.value === files.length)
+            bar.stop()
       })
   }
